371f453066574bcfb9181c99f7bb5a5906a18877
ogsambone <sgttrentz@gmail.com>
2018-01-31 18:31:49 -0600

    this is a test(getting familliar with source tree and git)

--- openzoomv2.2.py
+++ openzoomv2.2.py
@@ -14,7 +14,6 @@ import getpass
 #subst Z: C:\zoom
 #
 #if your having trouble with a string having invisible chars .rstrip() might help
-#test for github
 
 def config():
     '''Reads the config.zoom file, containing server names, length for 
